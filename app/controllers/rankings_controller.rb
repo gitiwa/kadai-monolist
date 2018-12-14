2b7cf810b7322ae532783309486d7ff6c0e793b2
ruby.jiro <cddpt903@yahoo.co.jp>
2018-12-14 18:05:31 +0000

    ranking3

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,11 +1,12 @@
 class RankingsController < ApplicationController
-  def want
-    @ranking_counts = Want.ranking
+
+  def have
+    @ranking_counts = Have.ranking
     @items = Item.find(@ranking_counts.keys)
   end
-  
-  def Have
-    @ranking_counts = Have.ranking
+
+  def want
+    @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
   end
   
